f510355d4f6266f2ef53d2bdfce36a998b3c66cc
Emran147 <emran.qedan11@hotmail.com>
2024-02-14 23:56:15 +0200

    more css improvments

--- frontend/src/components/Operations.jsx
+++ frontend/src/components/Operations.jsx
@@ -1,46 +1,57 @@
-import React, { useState } from 'react'
-import usePostRequest from '../customeHooks/post'
-import '../cssFiles/Operations.css'
+import React, { useState } from 'react';
+import usePostRequest from '../customeHooks/post';
+import '../cssFiles/Operations.css';
 
-export default function Operations({ handleBalance }) {
+const InputField = ({ name, value, placeholder, onChange }) => (
+    <input type={name === 'amount' ? 'number' : 'text'} name={name} value={value} placeholder={placeholder} onChange={onChange} />
+);
+
+const Operations = ({ handleBalance }) => {
     const [formData, setFormData] = useState({
         amount: '',
         vendor: '',
         category: ''
-    })
+    });
 
-    const { data, error, isLoading, sendPostRequest } = usePostRequest('http://localhost:8080/Transaction')
+    const { data, error, isLoading, sendPostRequest } = usePostRequest('http://localhost:8080/Transaction');
 
     const handleChange = (event) => {
         const { name, value } = event.target;
-        // Check if the value is numeric before updating the state
-        if (name === 'amount' && isNaN(value)) {
-            alert('Please enter a numeric value for the amount.');
-        } else {
-            setFormData(prevState => ({ ...prevState, [name]: value }))
-        }
-    }
+        setFormData(prevState => ({ ...prevState, [name]: value }));
+    };
 
-    const handleDeposit = () => {
-        sendPostRequest(formData)
-        handleBalance(formData.amount)
-    }
-    const handleWithdraw = () => {
-        const updatedFormData = { ...formData, amount: formData.amount * -1 }
-        sendPostRequest(updatedFormData)
-        handleBalance(updatedFormData.amount)
-    }
+    const handleSubmit = (isDeposit) => {
+        if (validateInputs()) {
+            const updatedFormData = isDeposit ? formData : { ...formData, amount: formData.amount * -1 };
+            sendPostRequest(updatedFormData);
+            handleBalance(updatedFormData.amount);
+            const newForm = {
+                amount: '',
+                vendor: '',
+                category: ''
+            }
+            setFormData(newForm)
+        } else {
+            alert('Please fill in all fields.');
+        }
+    };
+
+    const validateInputs = () => {
+        return Object.values(formData).every(value => value.trim() !== '');
+    };
 
     return (
-        <div className='Operations-container' >
-            <input type="number" name="amount" value={formData.amount} placeholder="Amount" onChange={handleChange} />
-            <input type="text" name="vendor" value={formData.vendor} placeholder="Vendor" onChange={handleChange} />
-            <input type="text" name="category" value={formData.category} placeholder="Category" onChange={handleChange} />
-            <button className='Deposit' onClick={handleDeposit} disabled={isLoading}>Deposit</button>
-            <button className='Withdraw' onClick={handleWithdraw} disabled={isLoading}>Withdraw</button>
+        <div className='Operations-container'>
+            <InputField name="amount" value={formData.amount} placeholder="Amount" onChange={handleChange} />
+            <InputField name="vendor" value={formData.vendor} placeholder="Vendor" onChange={handleChange} />
+            <InputField name="category" value={formData.category} placeholder="Category" onChange={handleChange} />
+            <button className='Deposit' onClick={() => handleSubmit(true)} disabled={isLoading}>Deposit</button>
+            <button className='Withdraw' onClick={() => handleSubmit(false)} disabled={isLoading}>Withdraw</button>
             {isLoading && <p>Loading...</p>}
             {error && <p>Error: {error}</p>}
             {data && <p>Success! Data received</p>}
         </div>
     );
-}
+};
+
+export default Operations;
