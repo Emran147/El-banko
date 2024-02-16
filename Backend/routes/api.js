8b0be3abe66d9c81d6081ed9f7c7acc78ded9ded
Emran147 <emran.qedan11@hotmail.com>
2024-02-16 09:59:31 +0200

    some backend cleaning

--- Backend/routes/api.js
+++ Backend/routes/api.js
@@ -1,21 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const externalAPi = require('./externalAPI')
-const weatherDataManager = require('../weatherDataManager')
-const CityWeather = require('../model/Weather')
 const Transaction = require('../model/Transaction')
-
-
-router.get("/Weather/:city", async function(req, res) {
-    try {
-      const city = req.params.city
-      let cityWeather = await externalAPi.getWeather(city);
-      cityWeather =  weatherDataManager.arrangeWeather(cityWeather)
-      res.send(cityWeather);
-    } catch (error) {
-      res.status(500).send({ error: 'Failed to fetch weather data' });
-    }
-  })
 
   // router.get("/Transactions/:category", async function(req, res) {
   //   const category = req.params.category;
@@ -30,8 +15,7 @@ router.get("/Weather/:city", async function(req, res) {
   //     res.status(500).send({ error: 'Failed to fetch transactions' });
   //   }
   // })
-  
-  
+    
   router.get("/Transactions/SumByCategory", async function(req, res) {
     try {
         Transaction.find({}).then(function(transactions) {
@@ -65,21 +49,7 @@ router.get("/Weather/:city", async function(req, res) {
         console.error("Failed to fetch transactions:", error);
         res.status(500).send({ error: 'Failed to fetch transactions' });
     }
-});
-
-  
-
-  
-router.get("/Weather/", function(req, res) {
-    try {
-      CityWeather.find({}).then(function (weather) {
-      res.send(weather)
-      })
-    } catch (error) {
-      res.status(500).send({ error: 'Failed to fetch weather data' });
-    }
-  })
-
+})
 
   router.get("/Transactions/", function(req, res) {
     try {
@@ -90,20 +60,6 @@ router.get("/Weather/", function(req, res) {
       res.status(500).send({ error: 'Failed to fetch weather data' });
     }
   })
-
-
-  router.post('/Weather', function (req, res) {
-    console.log(req.body)
-    const newWeather = new CityWeather(req.body)
-    newWeather.save()
-      .then((savedWeather) => {
-          res.status(201).json(savedWeather)
-      })
-      .catch((err) => {
-          console.error('Error saving expense:', err)
-          res.status(500).send('Error saving expense')
-      })
-})
 
 router.post('/Transaction', function (req, res) {
   console.log(req.body)
@@ -117,25 +73,6 @@ router.post('/Transaction', function (req, res) {
         res.status(500).send('Error saving expense')
     })
 })
-
-
-router.delete('/Weather/:cityName', async function (req, res) {
-  const { cityName  } = req.params
-  try {
-
-      const city = await CityWeather.findOneAndDelete({ name: cityName } )
-      if (!city) {
-          return res.status(404).send(`No city found`)
-      }
-
-      return res.send(`city  ${cityName} deleted`)
-
-  } catch (err) {
-      console.error('Error updating expense:', err)
-      return res.status(500).send('Error updating expense')
-  }
-})
-
 
 router.delete('/Transactions/:_id', async function (req, res) {
   const { _id  } = req.params
